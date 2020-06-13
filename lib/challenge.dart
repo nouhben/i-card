781057feecc8b0271316a9231c9b17356371b1d5
nouh ben kadi <benkadi.nouh@icloud.com>
2020-06-13 12:17:24 +0100

    Finnishing with listTile using cards Adding Devider

--- lib/challenge.dart
+++ lib/challenge.dart
@@ -36,54 +36,38 @@ class Challenge extends StatelessWidget {
             fontFamily: 'Montserrat',
           ),
         ),
-        Container(
-          margin: EdgeInsets.only(top: 5.0),
-          height: 2.0,
-          width: 200.0,
-          color: Colors.white,
+        SizedBox(
+          height: 20.0,
+          width: 150.0,
+          child: Divider(
+            color: Colors.white,
+          ),
         ),
         Card(
           color: Colors.white,
           margin: EdgeInsets.symmetric(horizontal: 25.0, vertical: 10.0),
-          child: Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Icon(
-                  Icons.call,
-                  size: 25.0,
-                  color: Colors.teal,
-                ),
-                SizedBox(width: 10.0),
-                Text(
-                  '+213-541-199-915',
-                  style: TextStyle(
-                    color: Colors.teal,
-                    fontSize: 15.0,
-                    fontWeight: FontWeight.w900,
-                    fontFamily: _fontFmilyName,
-                    letterSpacing: 2.0,
-                  ),
-                ),
-              ],
+          child: ListTile(
+            leading: Icon(Icons.call),
+            title: Text(
+              '+213-541-199-915',
+              style: TextStyle(
+                color: Colors.teal,
+                fontSize: 15.0,
+                fontWeight: FontWeight.w900,
+                fontFamily: _fontFmilyName,
+                letterSpacing: 2.0,
+              ),
             ),
           ),
         ),
-        Container(
-          color: Colors.white,
-          margin: EdgeInsets.symmetric(horizontal: 25.0, vertical: 10.0),
-          padding: EdgeInsets.all(10.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Icon(
+        Card(
+            margin: EdgeInsets.symmetric(horizontal: 25.0, vertical: 10.0),
+            child: ListTile(
+              leading: Icon(
                 Icons.email,
-                size: 25.0,
-                color: Colors.teal,
+                color: Colors.teal[600],
               ),
-              SizedBox(width: 10.0),
-              Text(
+              title: Text(
                 'ben.amine@dev.com',
                 style: TextStyle(
                   color: Colors.teal,
@@ -93,9 +77,38 @@ class Challenge extends StatelessWidget {
                   fontFamily: _fontFmilyName,
                 ),
               ),
-            ],
+            )),
+        /*
+        * OLD CODE TOO Heavy and complex
+        * Container(
+          //color: Colors.white, Cards comes with whit default color
+          margin: EdgeInsets.symmetric(horizontal: 25.0, vertical: 10.0),
+          child: Padding(
+            padding: const EdgeInsets.all(19.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Icon(
+                  Icons.email,
+                  size: 25.0,
+                  color: Colors.teal,
+                ),
+                SizedBox(width: 10.0),
+                Text(
+                  'ben.amine@dev.com',
+                  style: TextStyle(
+                    color: Colors.teal,
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.w900,
+                    letterSpacing: 2.5,
+                    fontFamily: _fontFmilyName,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
+        * */
       ],
     );
   }
